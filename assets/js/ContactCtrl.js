bfba3ed0f3dca80c51e0c8c7b5132f6e53fa0a4d
Tori Hedden <tori.hedden@gmail.com>
2016-03-21 17:25:58 -0400

    add basic layout

--- assets/js/ContactCtrl.js
+++ assets/js/ContactCtrl.js
@@ -1,4 +1,12 @@
 phoneApp.controller('ContactController', ['$scope', function($scope){
   $scope.test = "contact controller on board!"
 
+  $scope.contacts = [
+    {first: 'Tori', last: 'Hedden', number: '678-901-2345'},
+    {first: 'Roshani', last: 'Patel', number: '919-265-7890'},
+    {first: 'Matthew', last: 'Horowitz', number: '770-979-1234'},
+    {first: 'Bethany', last: 'Goodfred', number: '919-222-2220'},
+    {first: 'Rob', last: 'Gooding', number: '919-123-8790'}
+  ];
+
 }]);
